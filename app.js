ad770278745975ee9eebd32ef5db74597146bb6b
rudra <itsrudrapratapdev@gmail.com>
2023-01-11 19:25:03 +0530

    connect to local mongodb

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+
 const items = ["Buy food", "Cook food", "Eat food"];
 const workItems = [];
+
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+const Item = mongoose.model("Item", itemSchema);
+const first = new Item({name: "Welcome to your todo List"});
+const second = new Item({name: "Press the + button to add a new item"});
+Item.insertMany([first, second], function (err) {
+    if (err) {
+        console.log(err);
+    }else {
+        console.log("success")
+    }});
 
 app.get("/", (req, res) => {
     const day = date.getDate();
